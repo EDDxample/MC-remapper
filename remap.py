3683480c7f67daaece539f9d8900202f42ab07a2
EDDxample <EDDxample@gmail.com>
2019-09-04 18:08:27 +0200

    format classes

--- remap.py
+++ remap.py
@@ -3,6 +3,7 @@ import os, json, urllib.request
 def main():
     version = get_version()
     get_mappings(version)
+    reformat_mappings()
 
 def get_version():
     with open('version.config') as f:
@@ -11,10 +12,15 @@ def get_version():
 
 def get_mappings(version):
     jdir = os.getenv('APPDATA') + f'\\.minecraft\\versions\\{version}\\{version}.json'
+
+    if os.path.exists(jdir) and  os.path.isfile(jdir):
+        print(f'Found {version}.json')
+        return
+
     with open(jdir) as f:
         jfile = json.load(f)
         url = jfile['downloads']['client_mappings']['url']
-        
+
         print(f'Downloading the mappings for {version}...')
         download_file(url, 'mappings/client.txt')
         print('Done!')
@@ -22,7 +28,16 @@ def get_mappings(version):
 def download_file(url, out):
     urllib.request.urlretrieve(url, out)
 
-
+def reformat_mappings():
+    out = ''
+    with open('mappings/client.txt') as f:
+        for line in f.readlines()[1:]:
+            if line.startswith('    '): pass
+            else:
+                deobf_name, obf_name = line.split(' -> ')
+                new_line = f'CLASS\t{obf_name[:-2]}\t{deobf_name[:-1]}'
+                print(new_line)
+                out.join(new_line)
 
 
 
